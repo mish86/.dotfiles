d8394de8dbbe21e26bba1c2fab70cb45b8b4f65a
Mikhail Shestakov <mikhail.shestakov@netcracker.com>
2024-07-19 00:07:36 +0500

    feat(nvim): remaping of words jumping and line move

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -9,6 +9,11 @@ vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown, { desc = "tmux 
 vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp, { desc = "tmux Navigate Up" })
 vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight, { desc = "tmux Navigate Right" })
 
+vim.keymap.set("n", "<M-Left>", "b", { desc = "backward-word" })
+vim.keymap.set("n", "<M-Right>", "w", { desc = "forward-word" })
+vim.keymap.set("n", "<M-Up>", ":m -2<CR>", { desc = "move line up" })
+vim.keymap.set("n", "<M-Down>", ":m +1<CR>", { desc = "move line down" })
+
 local cmp = require("cmp")
 cmp.setup({
   completion = {
