ae2e01029e221be8211d12e4f7a7a09904c7994f
Mikhail Shestakov <mikhail.shestakov@netcracker.com>
2024-07-21 16:40:05 +0500

    feat(nvim): disable relative numbers

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.relativenumber = false -- Relative line numbers
+
 -- http://www.lazyvim.org/extras/lang/python#nvim-lspconfig
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
