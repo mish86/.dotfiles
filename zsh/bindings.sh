9731fc9ff489bf7d5e7961ba8eb3be52b8bfac15
Mikhail Shestakov <mikhail.shestakov@netcracker.com>
2024-07-25 22:57:21 +0500

    feat(zsh): history search backward and forward hotkeys

--- zsh/bindings.sh
+++ zsh/bindings.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# tmux fixes
-# https://stackoverflow.com/questions/6205157/how-to-set-keyboard-shortcuts-to-jump-to-beginning-end-of-line
-# https://stackoverflow.com/questions/12335787/with-iterm2-on-mac-how-to-delete-forward-a-word-from-cursor-on-command-line
-# bindkey '^[b' backward-word
-# bindkey '^[f' forward-word
-
 bindkey '^[[1;3D' backward-word
 bindkey '^[[1;3C' forward-word
 bindkey "^A" beginning-of-line
@@ -13,14 +7,12 @@ bindkey "^E" end-of-line
 bindkey '^[^?' backward-kill-word
 bindkey '^[[3;3~' kill-word
 
-#bindkey '^R' history-incremental-search-backward
-#bindkey '^S' history-incremental-search-forward
-# bindkey '^P' up-history
-# bindkey '^D' delete-char-or-list
+bindkey '^[[A' history-search-backward
+bindkey '^[[B' history-search-forward
 
-clear-scrollback-and-screen() {
-  zle clear-screen
-  tmux clear-history
-}
-zle -N clear-scrollback-and-screen
-bindkey '^[L' clear-scrollback-and-screen
+# clear-scrollback-and-screen() {
+#   zle clear-screen
+#   tmux clear-history
+# }
+# zle -N clear-scrollback-and-screen
+# bindkey '^[L' clear-scrollback-and-screen
