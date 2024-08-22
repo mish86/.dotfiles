fa06829f22a4d43aa2493aea63c782438bf775e3
Mikhail Shestakov <mikhail.shestakov@netcracker.com>
2024-08-22 14:39:52 +0500

    perf(nvim): vim options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,10 +2,18 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.relativenumber = false -- Relative line numbers
-
 -- http://www.lazyvim.org/extras/lang/python#nvim-lspconfig
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff_lsp"
+
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+-- vim.opt.relativenumber = false -- Relative line numbers
+vim.opt.scrolloff = 10 -- Lines of context
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.smarttab = true
