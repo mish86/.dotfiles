8432ae865d517b87df71ef9c1b11379a3d750f1b
Mikhail Shestakov <mikhail.shestakov@netcracker.com>
2024-10-26 18:46:31 +0500

    fix: pyenv swithed off, brew usage miniimized

--- zsh/python.sh
+++ zsh/python.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # python
-export PATH="$(brew --prefix python)/libexec/bin:$PATH"
+# export PATH="$(brew --prefix python)/libexec/bin:$PATH"
+export PATH="${HOMEBREW_PREFIX}/bin/python3:$PATH"
 
 # pipx
 # https://pipx.pypa.io/stable/
 export PATH=".:$PATH:$HOME/.local/bin"
-eval "$(register-python-argcomplete pipx)"
+# eval "$(register-python-argcomplete pipx)"
 
 # pyenv
+# https://github.com/pyenv/pyenv/issues/2918
 # https://github.com/pyenv/pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+# eval "$(pyenv init -)"
