f1c774237fd5fd290689ab4050c9509705475b61
Mikhail Shestakov <mikhail.shestakov@netcracker.com>
2025-02-28 10:23:29 +0500

    refactor(nvim): python black

--- nvim/lua/plugins/python.lua
+++ nvim/lua/plugins/python.lua
@@ -1,24 +1,70 @@
 return {
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "ninja", "rst" } },
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        pyright = {
-          settings = {
-            pyright = {
-              -- Using Ruff's import organizer
-              disableOrganizeImports = true,
+        ruff = {
+          cmd_env = { RUFF_TRACE = "messages" },
+          init_options = {
+            settings = {
+              logLevel = "error",
             },
-            python = {
-              analysis = {
-                -- Ignore all files for analysis to exclusively use Ruff for linting
-                ignore = { "*" },
-              },
+          },
+          keys = {
+            {
+              "<leader>co",
+              LazyVim.lsp.action["source.organizeImports"],
+              desc = "Organize Imports",
+            },
+          },
+        },
+        ruff_lsp = {
+          keys = {
+            {
+              "<leader>co",
+              LazyVim.lsp.action["source.organizeImports"],
+              desc = "Organize Imports",
             },
           },
         },
       },
+      setup = {
+        [ruff] = function()
+          LazyVim.lsp.on_attach(function(client, _)
+            -- Disable hover in favor of Pyright
+            client.server_capabilities.hoverProvider = false
+          end, ruff)
+        end,
+      },
     },
+  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       pyright = {
+  --         settings = {
+  --           pyright = {
+  --             -- Using Ruff's import organizer
+  --             disableOrganizeImports = true,
+  --           },
+  --           python = {
+  --             analysis = {
+  --               -- Ignore all files for analysis to exclusively use Ruff for linting
+  --               ignore = { "*" },
+  --             },
+  --           },
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
+  {
+    "nvim-neotest/neotest-python",
   },
   {
     "williamboman/mason.nvim",
@@ -27,9 +73,24 @@ return {
         "black",
         -- "isort",
         "ruff",
-        "pyright",
+        -- "pyright",
         "debugpy",
       })
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    -- stylua: ignore
+    keys = {
+      { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
+      { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
+    },
+    config = function()
+      if vim.fn.has("win32") == 1 then
+        require("dap-python").setup(LazyVim.get_pkg_path("debugpy", "/venv/Scripts/pythonw.exe"))
+      else
+        require("dap-python").setup(LazyVim.get_pkg_path("debugpy", "/venv/bin/python"))
+      end
     end,
   },
   -- http://www.lazyvim.org/extras/formatting/black
@@ -37,8 +98,8 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        python = { "black" },
-        -- python = { "ruff" },
+        -- python = { "black" },
+        python = { "ruff" },
       },
     },
   },
